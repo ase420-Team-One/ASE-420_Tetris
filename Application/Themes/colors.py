c92d6b50574417a728671445e200f1718ac68543
teddygizachew <teddygizachew1@gmail.com>
2022-12-02 20:17:49 -0500

    Add background colors

--- Application/Themes/colors.py
+++ Application/Themes/colors.py
@@ -19,10 +19,24 @@ class Colors:
         (255,175,25)        # honey yellow
     )
 
+    _background_colors = (
+        (255, 204, 255),
+        (255, 229, 204),
+        (204, 255, 229),
+        (255, 204, 229),
+        (204, 204, 255),
+        (229, 204, 255),
+        (204, 209, 255),
+        (204, 255, 255),
+        (204, 255, 204),
+    )
+
     def random(self):
         return random.randint(0, len(self._colors) -1)
 
     def select(self, index): return self._colors[index]
+
+    def select_background_color(self, index): return self._background_colors[index]
 
     def dark(self):
         self.PRIMARY=(23,22,26)         # eerie black
