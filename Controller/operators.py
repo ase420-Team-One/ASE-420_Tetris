4839821f20cdf6e4fbfb9dbdfc2b248fb7c0c87b
wolla22 <95152638+wolla22@users.noreply.github.com>
2022-12-03 21:55:46 -0500

    Update operators.py

--- Controller/operators.py
+++ Controller/operators.py
@@ -1,5 +1,8 @@
+from SoundControl.sound import Sound as sound
+
 class Operators:
     def rotate(tetrimino, board):
+        sound.block_move()
         old_val = tetrimino.rotation
         tetrimino.rotation += 1
         if board.intersects(tetrimino):
@@ -12,18 +15,21 @@ class Operators:
             tetrimino.rotation = old_val
 
     def go_left(tetrimino, board):
+        sound.block_move()
         old_val = tetrimino.shift_x
         tetrimino.shift_x -= 1
         if board.intersects(tetrimino):
             tetrimino.shift_x = old_val
 
     def go_right(tetrimino, board):
+        sound.block_move()
         old_val = tetrimino.shift_x
         tetrimino.shift_x += 1
         if board.intersects(tetrimino):
             tetrimino.shift_x = old_val
 
     def go_down(tetrimino, board):
+        sound.block_move()
         old_val = tetrimino.shift_y
         tetrimino.shift_y += 1
         if (board.intersects(tetrimino)):
